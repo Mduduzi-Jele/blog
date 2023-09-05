23f267e6ef6d91268a9881dbf358407ffa33584b
Mduduzi-Jele <jelemduduzi@gmail.com>
2023-09-05 22:47:35 +0200

    template

---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <div>Login</div>,
+  },
+  {
+    path: "/signup",
+    element: <div>Signup</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
