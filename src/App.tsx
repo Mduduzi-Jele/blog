b44970a6e1e61d3bdbd764991a805c0620a0f767
Mduduzi-Jele <jelemduduzi@gmail.com>
2023-09-06 11:58:23 +0200

    "updated template"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {createContext} from 'react'
+
+export const appContext = createContext(0);
+
 function App() {
- 
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <appContext.Provider value = {0}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>home</div>} />
+        <Route path="/about" element={<div>about</div>} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+    </appContext.Provider>
+  );
 }
 
-export default App
+export default App;
