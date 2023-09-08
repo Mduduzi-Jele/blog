04f4b0493e2f0670fbb09f71a543e24b731bcb9d
KabeloKgoadi <ksrkgoadi@gmail.com>
2023-09-08 11:06:48 +0200

    register version 1

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function SignUp() {
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const newUser = {
+      name,
+      email,
+      password,
+    };
+
+    let id = localStorage.length;
+    let userId = (++id).toString();
+    
+    localStorage.setItem(userId, JSON.stringify(newUser));
+
+    navigate('/');
+
+    setName('');
+    setEmail('');
+    setPassword('');
+  }
+ 
+  return (
+    <div>
+      <div className='header-text'>
+        <h1>Sign-Up</h1>
+      </div>
+
+      <div className='form-container'>
+        <form onSubmit={handleSubmit}>
+          <input type='text' 
+            className='text-area' 
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required>
+          </input> <br />
+          <br />
+          <input type='email' 
+            className='text-area' 
+            placeholder='Email'
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}>
+          </input> <br />
+          <br />
+          <input type='password' 
+            className='text-area' 
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}>
+          </input> <br />
+          <br />
+          <button className='btnSignUp'>Sign Up</button>
+        </form> 
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
