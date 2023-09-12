c91e68e35dd34fc143b0e6450efbcf5749f01bdc
chailock <chailock.sphe@gmail.com>
2023-09-12 14:28:58 +0200

    Create update

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { MyContext } from "../App";
-import "./CreatStyle.css";
 
 interface MyPostProps {}
 
