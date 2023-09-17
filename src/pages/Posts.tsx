1b38d5b872739e2e37b64a1beacf41d0466a4750
ThabangNWU <50927260+ThabangNWU@users.noreply.github.com>
2023-09-17 15:20:37 +0200

    filter is the post by duration done but without styling

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import Rating from './Rating';
+
 interface Post {
   title: string;
   message: string;
@@ -5,54 +8,100 @@ interface Post {
 }
 
 const Posts = () => {
-  const localstoragekeys = Object.keys(localStorage);
-  const posts: Post[] = [];
-  let user;
-  let sortedArray;
+  const [averageRating, setAverageRating] = useState<number | null>(null);
+  const [userRating, setUserRating] = useState<number>(0);
+  const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
+  const [filterDuration, setFilterDuration] = useState<string>('all'); // Default filter: All
 
-  const fetchPosts = () => {
-    localstoragekeys.map((userId) => {
-      user = JSON.parse(localStorage.getItem(userId));
-      if (user) {
-        if (user.myPosts) {
-          user.myPosts.map((post: Post) => {
-            posts.push(post);
-          });
-        }
-      }
-    });
+  const handleRatingChange = (newRating: number) => {
+    setAverageRating(newRating);
   };
 
-  fetchPosts();
+  useEffect(() => {
+    const localstoragekeys = Object.keys(localStorage);
+    const allPosts: Post[] = [];
 
-  if (user.myPosts) {
-    sortedArray = posts.sort((a, b) => {
+    const fetchPosts = () => {
+      localstoragekeys.forEach((userId) => {
+        const user = JSON.parse(localStorage.getItem(userId));
+        if (user && user.myPosts) {
+          user.myPosts.forEach((post: Post) => {
+            allPosts.push(post);
+          });
+        }
+      });
+    };
+
+    fetchPosts();
+
+    const now = new Date();
+    const oneWeekAgo = new Date(now);
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+    const oneMonthAgo = new Date(now);
+    oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+    const threeMonthsAgo = new Date(now);
+    threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
+
+    let filtered: Post[] = [];
+    
+    switch (filterDuration) {
+      case 'weekly':
+        filtered = allPosts.filter((post) => {
+          const postDate = new Date(post.dateTime);
+          return postDate >= oneWeekAgo;
+        });
+        break;
+      case '1month':
+        filtered = allPosts.filter((post) => {
+          const postDate = new Date(post.dateTime);
+          return postDate >= oneMonthAgo;
+        });
+        break;
+      case '3months':
+        filtered = allPosts.filter((post) => {
+          const postDate = new Date(post.dateTime);
+          return postDate >= threeMonthsAgo;
+        });
+        break;
+      case 'all':
+      default:
+        filtered = allPosts;
+        break;
+    }
+
+    const sortedArray = filtered.sort((a, b) => {
       if (a.dateTime && b.dateTime) {
-        return new Date(b.dateTime) - new Date(a.dateTime);
-      } else if (a.dateTime) {
-        return -1; // Move objects with 'a' having dateTime to the front
-      } else if (b.dateTime) {
-        return 1; // Move objects with 'b' having dateTime to the front
+        return new Date(b.dateTime).getTime() - new Date(a.dateTime).getTime();
       } else {
-        return 0; // Keep the order unchanged if neither have dateTime
+        return 0; 
       }
     });
-  }
 
-  console.log(sortedArray);
+    setFilteredPosts(sortedArray);
+  }, [filterDuration]);
 
   return (
     <div>
-      {user.myPosts ? (
+      <div>
+        <button onClick={() => setFilterDuration('all')}>All</button>
+        <button onClick={() => setFilterDuration('weekly')}>Weekly</button>
+        <button onClick={() => setFilterDuration('1month')}>1 Month</button>
+        <button onClick={() => setFilterDuration('3months')}>3 Months</button>
+      </div>
+      {filteredPosts.length > 0 ? (
         <div>
-          {sortedArray?.map((post, index) => {
-            return (
-              <div key={index}>
-                <p>Title: {post.title}</p>
-                <p>Message: {post.message}</p>
+          {filteredPosts.map((post, index) => (
+            <div key={index}>
+              <p>Title: {post.title}</p>
+              <p>Message: {post.message}</p>
+              <div>
+                <Rating initialRating={userRating} onRatingChange={handleRatingChange} />
+                {averageRating !== null && (
+                  <p>Average Rating: {averageRating.toFixed(2)}</p>
+                )}
               </div>
-            );
-          })}
+            </div>
+          ))}
         </div>
       ) : (
         <div>No posts added</div>
